37a51b751e0e63f3373e75075a4a3fd1cc9d080c
Geoff Ramseyer <geoff.ramseyer@cs.stanford.edu>
2024-05-10 11:37:01 -0700

    cleanup tlc

--- threadlocal/threadlocal_context.cc
+++ threadlocal/threadlocal_context.cc
@@ -16,63 +16,12 @@
 
 #include "threadlocal/threadlocal_context.h"
 
-#include "transaction_context/global_context.h"
-
-#include "transaction_context/transaction_context.h"
-#include "transaction_context/execution_context.h"
-
-#define TLC_TEMPLATE 
-#define TLC_DECL ThreadlocalContextStore
-
 namespace scs {
 
-TLC_TEMPLATE
 uint64_t
-TLC_DECL::get_uid()
+ThreadlocalContextStore::get_uid()
 {
     return cache.get().uid.get();
 }
-
-TLC_TEMPLATE
-void
-TLC_DECL::stop_rpcs()
-{
-    auto& ctxs = cache.get_objects();
-    for (auto& ctx : ctxs) {
-        if (ctx) {
-             ctx -> rpc.cancel_and_set_disallowed();
-        }
-    }
-}
-
-TLC_TEMPLATE
-void
-TLC_DECL::enable_rpcs()
-{
-    auto& ctxs = cache.get_objects();
-    for (auto& ctx : ctxs) {
-        if (ctx) {
-             ctx -> rpc.set_allowed();
-        }
-    }
-}
-
-#if USE_RPC
-
-TLC_TEMPLATE
-std::optional<RpcResult>
-TLC_DECL::send_cancellable_rpc(std::unique_ptr<ExternalCall::Stub>
-const& stub, RpcCall const& call)
-{
-    auto& ctx = cache.get();
-    uint64_t uid = ctx.uid.get();
-
-    return ctx.rpc.send_query(call, uid, stub);
-}
-
-#endif
-
-#undef TLC_DECL
-#undef TLC_TEMPLATE
 
 } // namespace scs
