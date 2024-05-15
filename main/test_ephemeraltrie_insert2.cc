4f3f2d5d4b05a621e39e84a9f8bcaa226c773aac
Geoff Ramseyer <geoff.ramseyer@cs.stanford.edu>
2024-05-15 00:12:44 -0700

    another test case

---
+++ main/test_ephemeraltrie_insert2.cc
@@ -0,0 +1,55 @@
+
+#include "libptimc.h"
+
+#include <cstdint>
+
+#include "hash_set/atomic_set.h"
+#include "crypto/hash.h"
+#include "config/yield_config.h"
+
+#include "xdr/storage.h"
+#include "mtt/ephemeral_trie/atomic_ephemeral_trie.h"
+
+using trie_t = trie::AtomicTrie<trie::EmptyValue, trie::UInt64Prefix, trie::EphemeralTrieMetadataBase, 5, 2>;
+
+static std::unique_ptr<trie_t> t;
+
+static void resetter() {
+    t.reset();
+}
+
+void *insert_worker1(void *ptr) {
+    trie::AtomicTrieReference ref(*t);
+    assert(ref.insert(trie::UInt64Prefix{0x0}));
+    return NULL;
+}
+
+void *insert_worker2(void *ptr) {
+    trie::AtomicTrieReference ref(*t);
+    assert(ref.insert(trie::UInt64Prefix{0x1}));
+    return NULL;
+}
+void *insert_worker3(void *ptr) {
+    trie::AtomicTrieReference ref(*t);
+    assert(ref.insert(trie::UInt64Prefix{0x10}));
+    return NULL;
+}
+
+
+void imc_check_main(void) {
+    scs::yield_config.EPHEMERALTRIE_YIELD = true;
+
+    register_resetter(resetter);
+    t = std::make_unique<trie_t>();
+
+    imcthread_t t1, t2, t3;
+    imcthread_create(&t1, NULL, insert_worker1, 0);
+    imcthread_create(&t2, NULL, insert_worker2, 0);
+    imcthread_create(&t3, NULL, insert_worker3, 0);
+
+    imcthread_join(t1, NULL);
+    imcthread_join(t2, NULL);
+    imcthread_join(t3, NULL);
+
+    assert(t->deep_sizecheck() == 3);
+}
